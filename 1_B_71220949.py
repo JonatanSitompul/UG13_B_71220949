72faccbc557eb598e4903570bf71e4f82a5dd80a
JonatanSitompul <117884416+JonatanSitompul@users.noreply.github.com>
2022-12-15 12:48:33 +0700

    Add files via upload

---
+++ 1_B_71220949.py
@@ -0,0 +1,40 @@
+import turtle as J
+
+J.pensize(20)
+J.color('black')
+
+J.forward(100)
+J.backward(200)
+J.forward(100)
+J.right(90)
+J.forward(120)
+J.circle(-60,200)
+
+
+J.exitonclick()
+
+# import for turtle module
+import turtle
+
+# making a workScreen
+ws = turtle.Screen()
+
+# defining a turtle instance
+geekyTurtle = turtle.Turtle()
+
+# iterating the loop 8 times
+for i in range(8):
+
+	# moving turtle 100 units forward
+	geekyTurtle.forward(100)
+	
+	# turning turtle 45 degrees so
+	# as to make perfect angle for an octagon
+	geekyTurtle.left(45)
+
+
+
+
+
+
+
